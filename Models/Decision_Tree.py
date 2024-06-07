9b4f273eef94e655cc88a2b0ef9c6f00d615fbd3
Ruoxuan Liu <ruli00004@stud.uni-saarland.de>
2024-06-07 10:38:04 +0200

    Update Decision_Tree.py

--- Models/Decision_Tree.py
+++ Models/Decision_Tree.py
@@ -3,8 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score,precision_score,recall_score
 
-def TimeSeriesDT(data, args): # the input should be features and label from the results of CNN
-    # now we use whole 58 data to train the CNN
+def TimeSeriesDT(data, args): # the input should be features and labels from the results of CNN
     X = data[0]
     y = data[1]
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=args.split, random_state=42)
@@ -19,10 +18,6 @@ def TimeSeriesDT(data, args): # the input should be features and label from the 
     print(len(y_pred))
     accuracy = accuracy_score(y_test, y_pred)
     print("Accuracy:", accuracy)
-
-    # result = compute_metrics(y_pred, y_test.numpy())  # 获得Acc和F1值
-    # print(result)
-
 
 def acc_and_f1(preds, labels):
     f1 = f1_score(y_true=labels, y_pred=preds, average='macro')
