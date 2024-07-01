dc7d387f2f20e5fa24ac12bead5f32d3319835e6
Kai Lange <kala01@dfki.de>
2024-07-01 15:58:28 +0200

    refactor

--- frontend/Dashboard.js
+++ frontend/Dashboard.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const barChart = new Chart(ctx, {
     type: "bar",
     data: {
-      labels: [], // Labels for the x-axis (time sequence)
+      labels: [],
       datasets: [
         {
           label: "Values",
-          data: [], // Initial empty data
+          data: [],
           backgroundColor: "rgba(54, 162, 235, 0.2)",
           borderColor: "rgba(54, 162, 235, 1)",
           borderWidth: 1,
@@ -34,7 +34,6 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 
-  // Funktion zum Abrufen der Daten mit Promise-Resolve
   function fetchData(resolve) {
     fetch("http://localhost:8000/data/data.txt")
       .then((response) => response.text())
@@ -42,25 +41,24 @@ document.addEventListener("DOMContentLoaded", function () {
         const values = data
           .split(",")
           .map((value) => parseInt(value.trim(), 10));
-        updateChart(values); // Funktion zum Aktualisieren des Charts aufrufen
-        resolve(); // Promise auflösen, um anzuzeigen, dass die Funktion abgeschlossen ist
+        updateChart(values);
+        resolve();
       })
       .catch((error) => {
         console.error("Fehler beim Abrufen der Daten:", error);
-        alert("Fehler beim Abrufen der Daten. Bitte versuchen Sie es erneut."); // Fehlerbehandlung
-        resolve(); // Promise auflösen, um anzuzeigen, dass die Funktion abgeschlossen ist (auch im Fehlerfall)
+        alert("Fehler beim Abrufen der Daten. Bitte versuchen Sie es erneut.");
+        resolve();
       });
   }
 
   function updateChart(values) {
     let index = 0;
-    const backgroundColors = []; // Array to store background colors
+    const backgroundColors = [];
 
     const intervalId = setInterval(() => {
       if (index < values.length) {
-        barChart.data.labels.push(index + 1); // Add time sequence label
+        barChart.data.labels.push(index + 1);
 
-        // Check condition and set background color
         if (values[index] > 5) {
           backgroundColors.push("red");
           const anomalyText = document.createElement("div");
@@ -74,47 +72,41 @@ document.addEventListener("DOMContentLoaded", function () {
           backgroundColors.push("grey");
         }
 
-        barChart.data.datasets[0].data.push(values[index]); // Add the value
-        barChart.data.datasets[0].backgroundColor = backgroundColors; // Update background colors array
+        barChart.data.datasets[0].data.push(values[index]);
+        barChart.data.datasets[0].backgroundColor = backgroundColors;
         barChart.update();
         index++;
       } else {
-        clearInterval(intervalId); // Stop the interval when data is exhausted
+        clearInterval(intervalId);
       }
     }, 100);
   }
 
-  // Funktion zum Hervorheben des Buttons mit Promise-Resolve
   function highlightButton(resolve) {
     const button = document.querySelector(".start-button");
 
-    // Ändere die Hintergrundfarbe auf grau
     button.style.backgroundColor = "#dcdcdc";
 
-    // Setze die Hintergrundfarbe nach einer kurzen Verzögerung zurück
     setTimeout(() => {
-      button.style.backgroundColor = "#2196f3"; // Originalfarbe wiederherstellen
-      resolve(); // Promise auflösen, um anzuzeigen, dass die Funktion abgeschlossen ist
-    }, 200); // Verzögerung in Millisekunden
+      button.style.backgroundColor = "#2196f3";
+      resolve();
+    }, 200);
   }
 
   function starting(resolve) {
     const infoBox = document.querySelector(".info-box");
 
-    // Nachrichten hinzufügen
     const messages = [
       "Drilling in Progress",
       "Anomaly-Detection is currently running",
     ];
 
-    // Nachrichten zur Info-Box hinzufügen
     messages.forEach((message) => {
       const messageElement = document.createElement("div");
       messageElement.textContent = message;
       infoBox.appendChild(messageElement);
     });
 
-    // Automatisch nach unten scrollen, wenn die Info-Box überläuft
     infoBox.scrollTop = infoBox.scrollHeight;
   }
 
@@ -126,7 +118,6 @@ document.addEventListener("DOMContentLoaded", function () {
         "Anomaly-Detection is currently running",
       ];
 
-      // running following functions simultaneously
       Promise.all([
         new Promise((resolve, reject) => {
           highlightButton(resolve);
